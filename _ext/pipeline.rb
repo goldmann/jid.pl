7eae200fa6dfa400a6e621566d2471c3e655e471
Marek Goldmann <marek.goldmann@gmail.com>
2012-04-22 18:02:26 +0200

    Added blog

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,20 @@
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/blog' ) 
+  extension Awestruct::Extensions::Posts.new('/blog') 
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom', :feed_title => "Wiadomości")
+  extension Awestruct::Extensions::IntenseDebate.new
+  extension Awestruct::Extensions::Tagger.new(:posts,
+                                              '/blog/index',
+                                              '/blog/tags',
+                                              :per_page=>5)
+
+  extension Awestruct::Extensions::TagCloud.new(:posts,
+                                                '/blog/tags/index.html',
+                                                :layout=>'base')
+
+  helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
---
+++ blog/2012-04-22-nowa-strona.md
@@ -0,0 +1,15 @@
+---
+title: "Nowa strona!"
+author: "Marek Goldmann"
+layout: blog
+timestamp: 2012-04-22t11:38:00.10+02:00
+tags: [ www ]
+---
+
+Od dzisiaj serwer [jid.pl](/) posiada nową stronę. Zapraszam do rzucenia okiem na stronę o [historii serwera](/history/), czy bardziej rozbudowaną [stronę o statystykach](/info/).
+
+Udostępniłem również [stronę kontaktową](/contact/), za pomocą której można zgłaszać problemy z działaniem serwera (i nie tylko).
+
+Na podstronie [pomoc](/help/) znajdziesz odpowiedzi na często zadawane pytania.
+
+Mam nadzieję, że nowa strona przypadnie wam do gustu!
